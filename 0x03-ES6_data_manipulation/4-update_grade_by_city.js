aba8ef9ec79e45a500fb8b02a0685e2087b4eea1
amirgambo <gambo.amir@yahoo.com>
2023-10-03 21:21:25 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,7 +3,7 @@ export default function updateStudentGradeByCity(arr, city, newGrades) {
     .filter(student => student.location === city)
     .map(student => {
       let gradeObj = newGrades.find(g => g.studentId === student.id);
-      let grade = gradeObj ? gradeObj.grade : null;
+      let grade = gradeObj ? gradeObj.grade : 'N/A'; // Set to 'N/A' if no grade found
       return { ...student, grade: grade };
     });
 }
